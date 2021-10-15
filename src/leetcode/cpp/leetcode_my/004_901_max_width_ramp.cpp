b62a8c4eb808da740dbe9c564559a8ce314d38ab
ww5365 <ww5365@163.com>
2021-10-15 14:48:21 +0800

    test2

---
+++ src/leetcode/cpp/leetcode_my/004_901_max_width_ramp.cpp
@@ -0,0 +1,46 @@
+
+/*
+ * Copyright (c) Technologies Co., Ltd. 2021-2021. All rights reserved.
+ * Description: 文件功能描述 
+
+ * 
+ * 给定一个整数数组 A，坡是元组 (i, j)，其中  i < j 且 A[i] <= A[j]. 这样的坡的宽度为 j - i. 找出 A 中的坡的最大宽度，如果不存在，返回0.
+
+ * 示例 1：
+ * 输入：[6,0,8,2,1,5]
+ * 输出：4
+ * 解释：
+ * 最大宽度的坡为 (i, j) = (1, 5): A[1] = 0 且 A[5] = 5.
+
+ * 
+ */
+
+#include "01_all.h"
+#include <stack>
+#include <vector>
+#include <iostream>
+
+class Solution {
+public:
+    int MaxWidthRamp(std::vector<int>& nums)
+    {
+
+    }
+
+};
+
+
+
+void TestMaxWidthRamp()
+{
+    std::vector<int> prices = {100, 80, 60, 70, 60, 75, 85};
+    StockSpanner *sp = new StockSpanner();
+    std::cout << "stock spanner test1: " << std::endl;
+    
+    for (auto e : prices) {
+        std::cout << sp->next(e) << " ";
+    }
+    std::cout << std::endl;
+    
+
+}
