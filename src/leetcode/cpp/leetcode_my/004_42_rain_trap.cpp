c2fd4338ce5713c19e78001e9740dc589b0c292a
veger <ww5365@163.com>
2023-10-10 16:48:19 +0800

    Update 004_42_rain_trap.cpp

--- src/leetcode/cpp/leetcode_my/004_42_rain_trap.cpp
+++ src/leetcode/cpp/leetcode_my/004_42_rain_trap.cpp
@@ -84,6 +84,113 @@ public:
 
 };
 
+// 2023.10.10
+
+class Solution {
+public:
+    int trap2(vector<int>& height) {
+
+        // 单调栈：单调递减，从栈底到栈顶 
+        stack<int> st;
+        int sum = 0;
+
+        for (int i = 0; i < height.size(); ++i) {
+            while(!st.empty() && height[st.top()] < height[i]) {
+                // 当前元素要大于栈顶，出栈处理
+                int curTop = st.top();
+                st.pop(); // 出栈
+
+                if (!st.empty()) {
+                    
+                    int minVal = std::min(height[i], height[st.top()]);
+                    int h = minVal - height[curTop];
+                    int steps = i - st.top() - 1;
+                    sum += (h * steps);
+                }
+            }
+            st.push(i); // 每个元素都入栈，要么空，要么height[i]<=栈顶元素
+        }
+        return sum;
+    }
+
+    int trap(vector<int>& height) {
+
+        // 参考： https://labuladong.github.io/algo/di-san-zha-24031/jing-dian--a94a0/ru-he-gao--0d5eb/
+
+        // 双指针
+
+        if (height.size() <= 2)
+            return 0; 
+
+        // 4个指针        
+        int left = 0; 
+        int right = height.size() - 1;
+        int lmax = height[0]; // 从0到left的最高柱子
+        int rmax = height[height.size() - 1]; //从right到end的最高柱子
+        
+        int result = 0;
+
+        while (left < right) {
+
+            lmax = std::max(lmax, height[left]);
+            rmax = std::max(rmax, height[right]);
+
+            if (lmax < rmax) {
+                result += (lmax  - height[left]);
+                left ++;
+            } else {
+                result += (rmax - height[right]);
+                right --;
+            }
+        }
+        return result;
+    }
+
+};
+
+/*
+11. 盛最多水的容器
+
+给定一个长度为 n 的整数数组 height 。有 n 条垂线，第 i 条线的两个端点是 (i, 0) 和 (i, height[i]) 。
+找出其中的两条线，使得它们与 x 轴共同构成的容器可以容纳最多的水。
+返回容器可以储存的最大水量。
+
+输入：[1,8,6,2,5,4,8,3,7]
+输出：49 
+解释：图中垂直线代表输入数组 [1,8,6,2,5,4,8,3,7]。在此情况下，容器能够容纳水（表示为蓝色部分）的最大值为 49。
+
+*/
+
+class Solution2 {
+public:
+    int maxArea(vector<int>& height) {
+
+        if (height.size() <= 1) 
+            return 0;
+
+        int left = 0;
+        int right = height.size() - 1;
+        int result = 0;
+
+        for (int i = 0; i < height.size(); ++i) {
+            
+            while (left < right) {
+                int steps = right - left;
+                if(height[left] < height[right]) {
+                    result = std::max(result, height[left] * steps);
+                    left ++;
+                } else {
+                    result = std::max(result, height[right] * steps);
+                    right --;
+                }
+            }
+        }
+
+        return result;
+    }
+};
+
+
 void TestRainTrap()
 {
     std::vector<int> vec = {4, 2, 0, 3, 2, 5};
