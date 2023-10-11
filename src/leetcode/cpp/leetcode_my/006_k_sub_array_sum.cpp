9d7a2b0ee33c384d4ab4b26d6aacc4ec3192cd73
veger <ww5365@163.com>
2023-10-11 11:22:12 +0800

    Update 006_k_sub_array_sum.cpp

--- src/leetcode/cpp/leetcode_my/006_k_sub_array_sum.cpp
+++ src/leetcode/cpp/leetcode_my/006_k_sub_array_sum.cpp
@@ -57,7 +57,7 @@ public:
         unordered_map<int, int> hash;
 
         int preSum = 0;
-        hash[0] = 1;
+        hash[0] = 1;  // 想明白点： map使用 和 初始值为0 presum - 0 代表了：[0,i] 之间的和
         int  result = 0;
 
         for (int i = 0; i < nums.size(); ++i) {
@@ -80,6 +80,81 @@ public:
 
 };
 
+
+/*
+
+1844 · 子数组和为K II
+https://www.lintcode.com/problem/1844/?showListFe=true&page=1&problemTypeId=2&tagIds=390&pageSize=50
+
+给定一个整数数组和一个整数k，你需要找到和为k的最短非空子数组，并返回它的长度。
+如果没有这样的子数组，返回-1.
+
+输入: 
+nums = [2,1,-1,4,2,-3] and k = 3
+输出: 
+2
+*/
+
+class Solution2 {
+public:
+    /**
+     * @param nums: a list of integer
+     * @param k: an integer
+     * @return: return an integer, denote the minimum length of continuous subarrays whose sum equals to k
+     */
+    int subarraySumEqualsKII(vector<int> &nums, int k) {
+        // write your code here
+
+        if (nums.size() <= 0) return -1;
+
+        unordered_map<int, int> hash; //记录达到这个前缀和时，最后元素的位置索引
+        
+        int sum = 0;
+        hash[0] = -1;  // 这个初始化理解很重要，保证有索引位置[0， i]之间的和
+        int result = std::numeric_limits<int>::max();
+
+        for (int i = 0; i < nums.size(); ++i) {
+
+            sum += nums[i];
+            int preSum = sum - k;
+
+            if (hash.find(preSum) != hash.end()) {
+                result = std::min(result, i - hash[preSum]);
+            }
+
+            hash[sum] = i;
+        }
+        if (result == std::numeric_limits<int>::max()) {
+            result = -1;
+        }
+
+        return result;
+    }
+};
+
+
+/*
+406 · 和大于S的最小子数组
+https://www.lintcode.com/problem/406
+给定一个由 n 个正整数组成的数组和一个正整数 s ，请找出该数组中满足其和 ≥ s 的最小长度子数组。如果无解，则返回 -1。
+
+输入: [2,3,1,2,4,3], s = 7
+输出: 2
+解释: 子数组 [4,3] 是该条件下的最小长度子数组。
+
+*/
+
+class Solution3 {
+public:
+    /**
+     * @param nums: an array of integers
+     * @param s: An integer
+     * @return: an integer representing the minimum size of subarray
+     */
+    int minimumSize(vector<int> &nums, int s) {
+        // write your code here
+    }
+};
 
 void testKSubArraySum()
 {
