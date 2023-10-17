3b943c76067515dbddae0e2268a81bd9ffcbdd26
veger <ww5365@163.com>
2023-10-17 20:36:06 +0800

    Update 007_1_two_sum.cpp

--- src/leetcode/cpp/leetcode_my/007_1_two_sum.cpp
+++ src/leetcode/cpp/leetcode_my/007_1_two_sum.cpp
@@ -1,5 +1,6 @@
 /*
  * 1. https://leetcode-cn.com/problems/two-sum/
+ * 56.https://www.lintcode.com/problem/56/solution/18046
  * problems:
  * Given an array of integers, find two numbers such that they add up to a specific target number.
  * The function twoSum should return indices of the two numbers such that they add up to the target, where index1 must be less than index2.
@@ -8,6 +9,7 @@
  * Input: numbers={2, 7, 11, 15}, target=9
  * Output:index1=1, index2=2
  *
+
  *解析思路：
  *1、o(n^2)的好想
  *2、？有没有更少复杂度？o(n)
@@ -18,6 +20,8 @@
 #include  <iostream>
 #include <unordered_map>
 #include <algorithm>
+
+#include <iterator>
 using namespace std;
 
 class Solution{
@@ -97,8 +101,66 @@ public:
     }
 };
 
+// 2023.10.17
 
-void two_sum(){
+class Solution2 {
+public:
+    /**
+     * @param numbers: An array of Integer
+     * @param target: target = numbers[index1] + numbers[index2]
+     * @return: [index1, index2] (index1 < index2)
+     */
+    vector<int> twoSum(vector<int> &numbers, int target) {
+        // write your code here
+        vector<int> index(numbers.size(), 0);
+        for (int i = 0; i < numbers.size(); ++i) {
+            index[i] = i;
+        }
+
+        // 按照numbers的值大小，进行索引排序
+        IndexSort(numbers, index);
+        
+        // std::copy(index.begin(), index.end(), std::ostream_iterator<int>(std::cout, " , "));
+        // std::cout << std::endl;
+
+        // 双指针
+
+        int left = 0;
+        int right = numbers.size() - 1;
+
+        vector<int> result(2, -1);
+
+        while (left < right) {
+            if (numbers[index[left]] + numbers[index[right]] > target) {
+                right --;
+            } else if(numbers[index[left]] + numbers[index[right]] < target) {
+                left ++;
+            } else {
+                break;
+            }
+        }
+
+        if (left != right) {
+            result[0] = std::min(index[left], index[right]);
+            result[1] = std::max(index[left], index[right]);
+        }
+        return result;
+    }
+
+private:
+    // 按照val大小，对索引进行排序
+
+    void IndexSort(const vector<int> &num, vector<int> &result) {
+
+        if (num.size() <= 1) 
+            return;
+        std::sort(result.begin(), result.end(), [&num](const int left, const int right)->bool{return num[left] < num[right];});
+        return ;
+    }
+
+};
+
+void TestTwoSum(){
 
     vector<int> data;
 
@@ -113,12 +175,19 @@ void two_sum(){
     Solution s;
     vector<int> res = s.twoSum2(data, 24);
 
-    cout << "twoSum result" << endl;
+    std::cout << "twoSum result" << endl;
     for(auto e:res){
-        cout << e << "  ";
+        std::cout << e << "  ";
     }
-    cout <<endl;
+    std::cout <<endl;
 
+    std::cout << "===============" << std::endl;
 
+    Solution2 s2;
+    vector<int> res2 = s2.twoSum(data, 24);
+
+    std::copy(res2.begin(), res2.end(), std::ostream_iterator<int>(std::cout, ","));
+    std::cout << std::endl;
 }
+
 
