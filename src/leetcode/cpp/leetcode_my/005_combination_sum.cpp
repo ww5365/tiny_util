53bd34bbc2628b1452277dfe74dfcd1468933ae0
veger <ww5365@163.com>
2023-10-20 10:40:36 +0800

    Update 005_combination_sum.cpp

--- src/leetcode/cpp/leetcode_my/005_combination_sum.cpp
+++ src/leetcode/cpp/leetcode_my/005_combination_sum.cpp
@@ -23,6 +23,109 @@
 
 using namespace std;
 
+
+
+// 10.20  参考dfs3，实现了子集和
+
+class Solution {
+
+public:
+    vector<vector<int>> Combination(vector<int> &vec)
+    {
+        vector<vector<int>> result;
+        if (vec.size() <= 0) {
+            return result;
+        }
+        vector<int> path;
+        path.reserve(vec.size());
+        dfs(vec, 0, path, result);
+        return result;
+    }
+
+
+private:
+
+    //  返回{1,2,3}的所有子集
+
+    void dfs(const vector<int> &vec, int start, vector<int> &path, vector<vector<int>> &result)
+    {
+
+        if (start >= vec.size()) {
+            result.push_back(path);
+            return;
+        }
+
+        // 兩個深搜分支
+        dfs(vec, start + 1, path, result);  // 不选当前元素
+        
+        path.push_back(vec[start]);   
+        dfs(vec, start + 1, path, result);  // 选择当前元素
+        path.pop_back();
+    }
+
+
+public:
+    // 有重复数字{1,2,2} 所有子集  注意：{2} {2} 是重复的，只能算1个
+    void dfs2(const vector<int> &vec, int start, vector<bool> &path, vector<vector<int>> &result) {
+
+        if (start >= vec.size()) {
+            vector<int> tmp;
+            for (int i = 0; i < path.size(); ++i) {
+                if (path[i])
+                    tmp.push_back(vec[i]);
+            }
+            result.push_back(tmp);
+            return;
+        }
+
+
+        // 选择当前元素，条件：前面的元素相同 && 没有被选择， 则当前重复元素已经被选择过,不选择
+
+        if (start == 0 || vec[start] != vec[start - 1] || (vec[start] == vec[start-1] && path[start - 1])) {
+            path[start] = true;
+            dfs2(vec, start + 1, path, result);
+            path[start] = false;
+        } 
+        
+        dfs2(vec, start + 1, path, result); //还是不选当前元素
+
+        return;
+    }
+
+public:
+    
+    // C ={12，7，5，3，2} 要求是：递减序列  target=7 找出所有子集合subset，使得sum(subset) = target;并且C中的每个元素都只能使用一次；
+    // https://www.lintcode.com/problem/153/   153 · 数字组合 II
+    
+    void dfs3(const vector<int> &vec, int start, int sum, vector<bool> &path, vector<vector<int>> &result) {
+
+
+        if (start >= vec.size()) {
+            if (sum == 0) {
+                vector<int> tmp;
+                for (int i = 0; i < path.size(); ++i){
+                    if (path[i]) {
+                        tmp.push_back(vec[i]);
+                    }
+                }
+                result.push_back(tmp);
+            }
+            return;
+        }
+        // 考虑去重 和 剪枝 传入的vec是先排序的
+        if (vec[start] <= sum && (start == 0 || vec[start] != vec[start - 1] || (vec[start] == vec[start-1] && path[start - 1]))) {
+            path[start] = true;
+            dfs3(vec, start + 1, sum - vec[start], path, result);
+            path[start] = false;
+        }
+
+        dfs3(vec, start + 1, sum, path, result);
+        return;
+    }
+};
+
+
+// 之前
 bool compare(int m,int n){
     return m>n;//升序排序
 }
