789466059d417866c35879e3de5e5210bcdf8d61
veger <ww5365@163.com>
2023-11-07 12:00:26 +0800

    Update 002_240_2d_matrix.cpp

--- src/leetcode/cpp/leetcode_my/002_240_2d_matrix.cpp
+++ src/leetcode/cpp/leetcode_my/002_240_2d_matrix.cpp
@@ -45,6 +45,48 @@ public:
 
 };
 
+// 2023.11.7 再刷一遍
+/*
+74: https://leetcode.cn/problems/search-a-2d-matrix/
+28: https://www.lintcode.com/problem/28/
+
+给你一个满足下述两条属性的 m x n 整数矩阵：
+每行中的整数从左到右按非严格递增顺序排列。
+每行的第一个整数大于前一行的最后一个整数。
+给你一个整数 target ，如果 target 在矩阵中，返回 true ；否则，返回 false
+
+end = m * n - 1
+mid -> mid/n mid%n
+
+ */
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int m = matrix.size();
+        int n = matrxi[0].size();
+        int start = 0;
+        int end = m * n - 1;
+
+        while (start < end) {
+            int mid = start + (end - start)/2;
+            int tmp = matrix[mid/n][mid%n];
+            if (tmp == target) {
+                return true;
+            } else if (tmp < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        // 最后的判断和while中条件相关
+        if (matrix[start/n][start%n] == target) {
+            return true;
+        }
+        return false;
+    }
+};
+
 void TestSerachMatrixII()
 {
     std::vector<vector<int>> vec = {{1,2,3,4}, {4, 8, 9 , 10}, {5, 9, 15, 16}};
